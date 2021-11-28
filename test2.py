912206d018cc5682c02027d3fe4d89b50be90561
andrei1112111 <74547328+andrei1112111@users.noreply.github.com>
2021-11-28 12:06:24 +0700

    28.11.21

--- test2.py
+++ test2.py
@@ -2,21 +2,32 @@ import cv2
 import pygame
 
 pygame.init()
-cap = cv2.VideoCapture('video.mp4')
-pygame.mixer.music.load("sound.mp3")
-success, img = cap.read()
-shape = img.shape[1::-1]
-wn = pygame.display.set_mode(shape)
-clock = pygame.time.Clock()
-pygame.mixer.music.play()
 
-while success:
-    clock.tick(24)
+
+def pre():
+    cap = cv2.VideoCapture('video.mp4')
+    pygame.mixer.music.load("sound.mp3")
     success, img = cap.read()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            success = False
-    wn.blit(pygame.image.frombuffer(img.tobytes(), shape, "BGR"), (0, 0))
-    pygame.display.update()
+    shape = img.shape[1::-1]
+    wn = pygame.display.set_mode(shape)
+    clock = pygame.time.Clock()
+    pygame.mixer.music.play()
+    while success:
+        clock.tick(24)
+        success, img = cap.read()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                success = False
+            if event.type == pygame.KEYDOWN and (event.key == pygame.K_ESCAPE):
+                return
 
+        wn.blit(pygame.image.frombuffer(img.tobytes(), shape, "BGR"), (0, 0))
+        pygame.display.update()
+
+
+try:
+    pre()
+except AttributeError:
+    pass
+print('ДАЛЕЕ')
 pygame.quit()
